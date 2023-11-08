32cafc5e8ab24015d6519d7ee359e60270f872a7
Cristian Lopez <cristian.lb.2002@gmail.com>
2023-11-07 18:47:05 -0800

    7 de noviemrbe

--- lib/presentation/widgets/chat/her_messages_bubble.dart
+++ lib/presentation/widgets/chat/her_messages_bubble.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyMessageBubble extends StatelessWidget {
-  const MyMessageBubble({super.key});
+class HerMessagesBubble extends StatelessWidget {
+  const HerMessagesBubble({super.key});
 
   @override
   Widget build(BuildContext context) {
